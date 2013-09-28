5f05219ddf226bf74c76152d8489ada364d2e36e
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-09-28 08:42:19 +0200

    external libs

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.7"]]
+                 [compojure "1.1.5"]
+                 [enfocus "1.0.1"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-ring "0.8.7"]
+            [lein-cljsbuild "0.3.3"]]
   :ring {:handler checkmate.core/app})
