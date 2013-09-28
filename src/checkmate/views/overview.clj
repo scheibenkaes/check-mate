22dda537c409126e85ea9108fa26b17e1c22c6a0
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-09-28 22:00:27 +0200

    sorting bug

--- src/checkmate/views/overview.clj
+++ src/checkmate/views/overview.clj
@@ -7,12 +7,12 @@
     [:div
      (for [[letter items] ordered]
        [:div
-        [:h4 (string/upper-case letter)]
+        [:h4 letter]
         (for [i items]
           [:div (elm/link-to (str "/show/" (:_id i)) (:name i))])])]))
 
 (defn render [lists]
-  (let [grouped (group-by (comp first :name) lists)]
+  (let [grouped (group-by (comp first string/upper-case :name) lists)]
     {:body [:div
             [:h3 "All checklists"]
             (render-group grouped)]}))
