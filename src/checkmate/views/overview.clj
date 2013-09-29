158001d1ba7c05fb38a1d79004d953137a83d33f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-09-29 18:27:58 +0200

    no more badges

--- src/checkmate/views/overview.clj
+++ src/checkmate/views/overview.clj
@@ -8,22 +8,23 @@
     [:div
      (for [[letter items] ordered]
        [:div
-        [:h4 letter]
+        [:h3 letter]
         (for [i items]
           (let [num-done (count (filter :checked? (:items i)))
                 num-all (count (:items i))
                 cls-link (keyword (if-not (= num-all num-done) "strong" "strong.checked"))]
             [(keyword (format "div#%s.row" (:_id i)))
-             [:div.col-md-4
-              [cls-link (elm/link-to (str "/show/" (:_id i)) (:name i)) " " [:span.badge (str num-done "/" num-all)]]]
-             [:div.col-md-8
-              [:div.btn-group
-               [:a {:class "btn btn-default btn-xs" :href (str "/edit/" (:_id i))} [:span.glyphicon.glyphicon-edit] "Edit"]
-               [:a {:class "btn btn-default btn-xs":href "#" :onclick (format "checkmate.views.overview.try_delete_list('%s', '%s');" (:_id i) (:name i))} [:span.glyphicon.glyphicon-trash] "Delete"]]]]))])]))
+             [:p
+              [:div.col-md-8
+               [cls-link (elm/link-to (str "/show/" (:_id i)) (str (:name i) "  (" num-done "/" num-all ")"))]]
+              [:div.col-md-4
+               [:div.btn-group
+                [:a {:class "btn btn-default btn-xs" :href (str "/edit/" (:_id i))} [:span.glyphicon.glyphicon-edit] "Edit"]
+                [:a {:class "btn btn-default btn-xs":href "#" :onclick (format "checkmate.views.overview.try_delete_list('%s', '%s');" (:_id i) (:name i))} [:span.glyphicon.glyphicon-trash] "Delete"]]]]]))])]))
 
 (defn render [lists]
   (let [grouped (group-by (comp first string/upper-case :name) lists)]
-    {:body [:div
+    {:body [:div.container
             [:h3 "All checklists"]
             [:div.row
              [:div.col-md-12
