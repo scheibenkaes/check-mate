53cfa91dbc92fca0937015b51ef18e10ff5b2232
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-09-29 16:09:30 +0200

    add badges to a-z
    indication the progress

--- src/checkmate/views/overview.clj
+++ src/checkmate/views/overview.clj
@@ -10,13 +10,16 @@
        [:div
         [:h4 letter]
         (for [i items]
-          [(keyword (format "div#%s.row" (:_id i)))
-           [:div.col-md-4
-            [:strong (elm/link-to (str "/show/" (:_id i)) (:name i))]]
-           [:div.col-md-8
-            [:div.btn-group
-             [:a {:class "btn btn-default btn-xs" :href (str "/edit/" (:_id i))} [:span.glyphicon.glyphicon-edit] "Edit"]
-             [:a {:class "btn btn-default btn-xs":href "#" :onclick (format "checkmate.views.overview.try_delete_list('%s', '%s');" (:_id i) (:name i))} [:span.glyphicon.glyphicon-trash] "Delete"]]]])])]))
+          (let [num-done (count (filter :checked? (:items i)))
+                num-all (count (:items i))
+                cls-link (keyword (if-not (= num-all num-done) "strong" "strong.checked"))]
+            [(keyword (format "div#%s.row" (:_id i)))
+             [:div.col-md-4
+              [cls-link (elm/link-to (str "/show/" (:_id i)) (:name i)) " " [:span.badge (str num-done "/" num-all)]]]
+             [:div.col-md-8
+              [:div.btn-group
+               [:a {:class "btn btn-default btn-xs" :href (str "/edit/" (:_id i))} [:span.glyphicon.glyphicon-edit] "Edit"]
+               [:a {:class "btn btn-default btn-xs":href "#" :onclick (format "checkmate.views.overview.try_delete_list('%s', '%s');" (:_id i) (:name i))} [:span.glyphicon.glyphicon-trash] "Delete"]]]]))])]))
 
 (defn render [lists]
   (let [grouped (group-by (comp first string/upper-case :name) lists)]
